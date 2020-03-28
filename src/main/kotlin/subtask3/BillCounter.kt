b68515fd31168b23387960a36831ecbe6835caad
Viktor Yushkevich <viktor.yushkevich@vrpconsulting.com>
2020-03-29 00:51:11 +0300

    feat: submit third task

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -1,10 +1,12 @@
 package subtask3
 
 class BillCounter {
-
-    // TODO: Complete the following function
-    // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val realBill = (bill.sum() - bill[k]) / 2
+        if (realBill == b) {
+            return "Bon Appetit"
+        } else {
+            return (b - realBill).toString()
+        }
     }
 }
