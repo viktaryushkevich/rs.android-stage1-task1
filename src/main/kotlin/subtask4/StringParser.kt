76b01667f737aac22fd96bf8ec27d8dce67b3dfd
Viktor Yushkevich <viktor.yushkevich@vrpconsulting.com>
2020-03-29 00:59:21 +0300

    feat: submit task

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -1,9 +1,26 @@
 package subtask4
 
 class StringParser {
-
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val opnBrackets = listOf('(', '<', '[')
+        val clsBrackets = listOf(')', '>', ']')
+
+        val result = ArrayList<String>()
+        val string = StringBuilder(inputString)
+        var lstBracket = 0
+        for (i in string.length - 1 downTo 0) {
+            if (opnBrackets.contains(string[i])) {
+                for (j in i until string.length) {
+                    if (string[j] == clse]Brackets[opnBrackets.indexOf(inputString[i])]) {
+                        lastBracket = j
+                        string[lastBracket] = '_'
+                        string[i] = '_'
+                        break
+                    }
+                }
+                result.add(0, inputString.substring(i + 1, lastBracket))
+            }
+        }
+        return result.toTypedArray()
     }
 }
