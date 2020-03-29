373b8ce4a2a8ed2c93f745b98f7984f7a0891eb2
Viktor Yushkevich <viktor.yushkevich@vrpconsulting.com>
2020-03-29 11:09:08 +0300

    feat: submit fifth task

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -1,9 +1,48 @@
 package subtask5
 
 class HighestPalindrome {
-
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        var counter = 0
+        for (i in 0 until n / 2) {
+            if (digitString[i] != digitString[n - i - 1]) {
+                counter++
+            }
+        }
+
+        if (counter > k) {
+            return "-1"
+        }
+        val result = digitString.toMutableList()
+
+        if (counter == 1 && n % 2 == 1) {
+            result[n / 2] = '9'
+        }
+
+        for (i in 0 until n / 2) {
+            println(digitString[i] + ":" + digitString[n - i - 1])
+            if (digitString[i] < digitString[n - i - 1]) {
+                println(digitString[i])
+                result[i] = digitString[n - i - 1]
+                counter--
+            } else if (digitString[i] > digitString[n - i - 1]) {
+                result[n - 1 - i] = result[i]
+                counter--
+            }
+        }
+
+        counter = k - counter
+        var i = 0
+        while (counter > 1) {
+            if (result[i] != '9') {
+                result[i] = '9'
+                result[n - 1 - i] = '9'
+                counter -=2
+            }
+            i++
+        }
+
+        val stringBuilder = StringBuilder()
+        result.forEach { stringBuilder.append(it) }
+        return stringBuilder.toString()
     }
 }
